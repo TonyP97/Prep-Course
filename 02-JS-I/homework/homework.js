3402e0749595a3175c72bf21a55e8ff132cfe99c
unknown <agustinpincheira97@gmail.com>
2022-05-06 20:27:07 -0300

    tarea java casi casi

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -163,6 +163,9 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+  if (numero > 0) {return ("Es positivo")}
+  else if (numero < 0) {return ("Es negativo")}
+  else return false//return false (antes estaba así)
 }
 
 function agregarSimboloExclamacion(str) {
